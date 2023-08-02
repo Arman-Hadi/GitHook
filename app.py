df02bd97d80bd7b806561f7579a63bfaab66da08
Arman Hadi <armanhadi728@gmail.com>
2023-08-02 16:18:30 +0330

    add auth

--- app.py
+++ app.py
@@ -1,8 +1,43 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.exceptions import HTTPException
+import hashlib
+import hmac
+from datetime import datetime
+
+
+def log(_log):
+    with open('log.log', 'a') as f:
+        f.write(
+            str(datetime.now()) + ':\n' + _log + '\n'
+        )
+
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+    
+    Raise and return 403 if not authorized.
+    
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise HTTPException(status_code=403, detail="Request signatures didn't match!")
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+secret = "apihoook_aslfjasdwevn2408"
+
+@app.route("/apihook", methods=['POST',])
+def apihook():
+    payload = request.get_data()
+    log(str(payload))
+
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
