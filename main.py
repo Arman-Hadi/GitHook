c6b43b40ce6aacbf202644c2ff1186bd36436d08
Arman Hadi <armanhadi728@gmail.com>
2023-10-05 13:18:20 +0330

    log fuck

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ def apihook():
         payload = request.get_data()
         sig_header = request.headers.get('x-hub-signature-256', '')
         verify_signature(payload, 'apihoook_aslfjasdwevn2408', sig_header)
+        log('heyadsfladsjfaldk;sfjl;adsj')
 
         data = request.get_json()
         if data['repository']['full_name'] == 'BracketAcademy/BracketAcademy':
