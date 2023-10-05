1d12580a1c6974e69608a9b0104d4609c33be19e
Arman Hadi <armanhadi728@gmail.com>
2023-10-05 12:52:58 +0330

    add konj instagram hook

--- main.py
+++ main.py
@@ -74,6 +74,12 @@ def apihook():
             run_command('git pull https://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@github.com/BracketAcademy/BracketAcademy.git main', cwd)
             run_command("docker compose down", cwd)
             run_command("docker compose up -d", cwd)
+        elif data['repository']['full_name'] == 'BracketAcademy/konj-instagram':
+            cwd = '/root/w/KonjInstagram/front'
+            run_command('git pull https://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@github.com/BracketAcademy/konj-instagram.git main', cwd)
+            run_command('npm run build')
+            # run_command("docker compose down front", cwd)
+            # run_command("docker compose up front -d", cwd)
     except Exception as e:
         log_error(e)
         return str(e), 400
