294db34d0c0b746c3b2ed72ff882bad0a3e3ace6
Arman Hadi <armanhadi728@gmail.com>
2023-10-16 12:50:02 +0330

    change feedlink

--- main.py
+++ main.py
@@ -70,16 +70,14 @@ def apihook():
 
         data = request.get_json()
         if data['repository']['full_name'] == 'BracketAcademy/BracketAcademy':
-            # run_command('cd /root/w/Bracket/backend')
             cwd = '/root/w/Bracket/backend'
             run_command('git pull https://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@github.com/BracketAcademy/BracketAcademy.git main', cwd)
             run_command("docker compose down", cwd)
             run_command("docker compose up -d", cwd)
         elif data['repository']['full_name'] == 'BracketAcademy/konj-instagram':
-            log('heyadsfladsjfaldk;sfjl;adsj')
-            cwd = '/root/w/Bracket/KonjInstagram/front'
+            log('--------------into if --------------------')
+            cwd = '/root/w/FeedLink/front'
             run_command('git pull https://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@github.com/BracketAcademy/konj-instagram.git master', cwd)
-            run_command('npm run build')
             # run_command("docker compose down front", cwd)
             # run_command("docker compose up front -d", cwd)
     except Exception as e:
