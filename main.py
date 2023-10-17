a8ad8a07b0cd3bde1bc9e79cad304a3590bf25a0
Arman Hadi <armanhadi728@gmail.com>
2023-10-17 19:48:38 +0330

    ba

--- main.py
+++ main.py
@@ -69,8 +69,9 @@ def apihook():
 
         data = request.get_json()
         if data['repository']['full_name'] == 'BracketAcademy/BracketAcademy':
-            cwd = '/root/w/BracketAcademy/backend/'
-            run_command('git pull https://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@github.com/BracketAcademy/BracketAcademy.git main', cwd)
+            cwd = '/root/w/BracketAcademy/backend'
+            # run_command('git pull https://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@github.com/BracketAcademy/BracketAcademy.git main', cwd)
+            run_command('mkdir hell')
             run_command("docker compose down", cwd)
             run_command("docker compose up -d", cwd)
             return 'OK BRACKET'
